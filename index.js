b845df99274f88b0abdb0837b27fc073a9d3e0c8
Ruan Santiago <ruanvansantiago@gmail.com>
2021-11-30 21:16:53 -0300

    Função de Depósito adicionada

--- index.js
+++ index.js
@@ -29,7 +29,24 @@ function operation() {
 
             if (action === 'Criar conta') {
                 createAccount()
+            } else if (action === 'Consultar Saldo') { }
+
+
+            else if (action === 'Depositar') {
+
+                deposit()
+
             }
+
+            else if (action === 'Sacar') {
+
+            }
+
+            else if (action === 'Sair') {
+            console.log('Obrigado por utilizar o Account Bank')
+            process.exit()
+            }
+
 
         })
         .catch((err) => console.log(err))
@@ -83,3 +100,92 @@ function buildAccount() {
     }).catch((err) => { console.log(err) })
 
 }
+
+
+// Função que coloca uma quantia de dinheiro na conta do usuário
+
+function deposit(){
+
+    inquirer.prompt([{
+        name: 'accountName',
+        message:'Qual o nome da sua conta?'
+    }]).then((answer)=>{
+
+        // Aqui irei veridicar se a conta do usuário está no banco de dados
+        const accountName = answer['accountName']
+        if(!checkAccount(accountName)){
+            return deposit()
+        }
+
+        inquirer.prompt([{
+            name: 'amount',
+            message: 'Valor do depósito: '
+        }]).then(((answer) =>{
+
+            const amount = answer['amount']
+            
+            addAmount(accountName,amount)
+            
+
+            operation()
+
+
+        })).catch((err)=>{console.log(err)})
+
+
+    }).catch((err)=> {console.log(err)})
+}
+
+
+
+//funcao que verifica se a conta existe
+
+function checkAccount(accountName){
+
+    if(!fs.existsSync(`accounts/${accountName}.json`)){
+        console.log('Esta conta nao existe, tente novamente')
+        return false
+    }
+
+    return true
+}
+
+// função que coloca o montante do valor no nome de uma conta 
+function addAmount(accountName,amount){
+
+    const accountData = getAccount(accountName) // aqui temos a conta em objeto
+
+
+    if(!amount){
+        console.log('ocorreu um erro, tente novamente')
+        return deposit()
+    }
+
+    // acesso o valor do balance  da conta caso o valor exista
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
+
+    // Agora preciso salvar isso no arquivo
+
+    fs.writeFileSync(`accounts/${accountName}.json`,
+    JSON.stringify(accountData), //transformo o JSON em texto,
+    function (err){console.log(err)},  // função callback para caso exista um erro
+    )
+
+    console.log(`Foi depositado o valor de ${amount}R$`)
+}
+
+// funcao que pega a conta
+
+function getAccount(accountName){
+    //Aqui pegamos a função em JSON
+
+
+
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`,{ 
+        encoding:'utf8', // o tipo de enconding utilizando no brasil
+        flag:'r' //essa flag nos diz ao programa que só queremos ler o arquivo
+
+    })
+    return JSON.parse(accountJSON) // aqui transformamos o texto em arquivo JSON
+}
+
